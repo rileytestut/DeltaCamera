a66f0248368c5d42ca258bc37a999b75be95da9c
Riley Testut <riley@rileytestut.com>
2025-04-29 15:44:33 -0500

    Initial Commit

---
+++ Delta Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Delta Camera
+//
+//  Created by Riley Testut on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Delta Camera/Delta_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delta_CameraApp.swift
+//  Delta Camera
+//
+//  Created by Riley Testut on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Delta_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
